2715002af89ba7ad1cee69b4cd19c46b500c7d8d
xavicp <xavier-aleix.cristofol@estudiantat.upc.edu>
2026-05-16 21:01:16 +0200

    A2

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject warningPanel;
+    public GameObject gameplayHUD;
+    public GameObject instructionsPanel;
+    public GameObject recordsPanel;
+
+    // BOTÓN JUGAR
+    public void PlayGame()
+    {
+        mainMenuPanel.SetActive(false);
+        warningPanel.SetActive(true);
+    }
+
+    // BOTÓN "ESTOY LISTO"
+    public void StartGameplay()
+    {
+        warningPanel.SetActive(false);
+        gameplayHUD.SetActive(true);
+    }
+
+    // BOTÓN INSTRUCCIONES
+    public void OpenInstructions()
+    {
+        mainMenuPanel.SetActive(false);
+        instructionsPanel.SetActive(true);
+    }
+
+    // VOLVER DESDE INSTRUCCIONES
+    public void BackToMainMenu()
+    {
+        instructionsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    // BOTÓN RECORDS
+    public void OpenRecords()
+    {
+        mainMenuPanel.SetActive(false);
+        recordsPanel.SetActive(true);
+    }
+
+    // VOLVER DESDE RECORDS
+    public void BackFromRecords()
+    {
+        recordsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+}
